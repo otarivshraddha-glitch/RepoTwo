78bdf469761525810b35a484ac7df09f292841ac
otarivshraddha-glitch <otari.vshraddha@gmail.com>
2026-05-15 10:56:45 +0530

    Add files via upload

---
+++ EvenNo.java
@@ -0,0 +1,22 @@
+package HomeWorkSet;
+
+public class EvenNo {
+
+	public static void main(String[] args)
+	{
+	    //Q3. Print even numbers between 1 to 20. 
+		
+		
+		
+	        int i = 2;   // Start from first even number
+
+	        while (i <= 20) 
+	        {
+	        	System.out.println("Even numbers are...." +i);
+	        	//System.out.println(i);
+	            i += 2;   // Increase by 2 to get next even number
+	        }
+
+	}
+
+}
