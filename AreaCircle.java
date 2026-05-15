ed0e93099686a1a044121d1bf086e65f1d2616c7
otarivshraddha-glitch <otari.vshraddha@gmail.com>
2026-05-15 11:52:13 +0530

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,23 @@
+package HomeWorkSet1;
+
+public class AreaCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Q.5 Area of Circle Write a program to calculate the area of a circle. 
+		//Formula: Area = 3.14 × radius × radius 
+		
+		  // Given value
+        double radius = 7;   // Radius of the circle
+
+        // Formula: Area = 3.14 × radius × radius
+        double area = 3.14 * radius * radius;
+
+        // Display result
+        System.out.println("Radius: " + radius);
+        System.out.println("Area of Circle: " + area);
+
+	}
+
+}
